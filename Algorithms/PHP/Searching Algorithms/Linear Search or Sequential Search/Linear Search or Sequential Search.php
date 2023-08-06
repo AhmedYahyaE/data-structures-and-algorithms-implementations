698c7586b1a312870dcc7e988b4ab6810292ca59
Ahmed Yahya <a.yahya1@yahoo.com>
2023-08-06 03:53:17 +0300

    Linear Search / Sequential Search

---
+++ Algorithms/PHP/Searching Algorithms/Linear Search or Sequential Search/Linear Search or Sequential Search.php
@@ -0,0 +1,22 @@
+<?php
+// Linear Search / Sequential Search Algorithm
+// The Linear Search algorithm is best suited for small lists or when the list is not sorted (unsorted lists)
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+function linearSearchOrSequentialSearch($target, $array) {
+    $array_length = count($array);
+
+    for ($i = 0; $i < $array_length; $i++) {
+        if ($array[$i] == $target) {
+            return $i; // $target found! Return its index
+        }
+    }
+
+
+    return false; // $target not found
+}
+
+
+
+echo '<pre>', var_dump(linearSearchOrSequentialSearch(3, [10, 5, 3, 8, 2, 7])), '</pre>';
